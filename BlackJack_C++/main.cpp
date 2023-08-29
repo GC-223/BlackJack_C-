e5c81d2cbe5c78167a0e97a0d6636d29a53e5235
GC-223 <gc223odin@gmail.com>
2023-08-28 21:16:41 -0700

    create Card structure that holds the suit and rank of a card

--- BlackJack_C++/main.cpp
+++ BlackJack_C++/main.cpp
@@ -5,10 +5,67 @@
 //  Created by Greyson Chavez on 8/28/23.
 //
 
+
+
+// What are all of the things that we need for the initial set up of the game?
+
+// A deck - An array of cards
+    // A card
+        // A suit
+        // A number/rank
+
+
+#include <array>
 #include <iostream>
+#include <string>
+
+// we need the different types of suits
+// Relatively few, and they are enumerated
+enum class CardSuit
+{
+    spade ,
+    heart ,
+    diamond ,
+    club ,
+    
+    suitMax ,
+} ;
+
+// We now need the ranks
+enum class CardRank
+{
+    one ,
+    two ,
+    three ,
+    four ,
+    five ,
+    six ,
+    seven ,
+    eight ,
+    nine ,
+    ten ,
+    jack ,
+    queen ,
+    king ,
+    ace ,
+    
+    rankMax ,
+} ;
+
+// so lets start with a card structure
+struct Card
+{
+    CardSuit suit { } ;
+    CardRank rank { } ;
+} ;
+
+
 
 int main(int argc, const char * argv[])
 {
+    
+    // now we want our deck
+    // the deck is going to be an array of cards
     
     
     return 0;
